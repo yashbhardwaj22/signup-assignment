638507a394dc45137fd445a042cd77ebaab68c78
yash <yashbhardwaj95@gmail.com>
2018-07-31 20:14:12 +0530

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl , FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms'; 
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'my-form';
+  myFormGroup=new FormGroup({
+    fname:new FormControl('',[
+      Validators.required,
+      Validators.pattern("[a-zA-Z]+$")
+    ]),
+    lname:new FormControl('',[
+      Validators.required,
+      Validators.pattern("[a-zA-Z]+$")
+    ]),
+    gender:new FormControl('',[
+      Validators.required,
+      Validators.pattern("(M)|(m)|(F)|(f)")
+    ]),
+    contactNo:new FormControl('',[
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(10),
+      Validators.pattern("[0-9]+$")
+    ]),
+    password:new FormControl('',[Validators.required, Validators.pattern("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~`!@#$%^&*()_-{}<>,./?:;'+=]).{8,16}")]),
+    confirmPassword:new FormControl('',[Validators.required]),
+    empId:new FormControl('',[Validators.required,
+    Validators.minLength(4),
+    Validators.maxLength(4),
+    Validators.pattern("[0-9]+$")
+    ])
+  }
+);
+get f() { return this.myFormGroup.controls; }
+
+onSubmit(){
+  localStorage.setItem("json",JSON.stringify(this.myFormGroup.value));
+  console.warn(this.myFormGroup.value);
 }
+}
+
